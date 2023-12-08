83eb27f52ba21dd8a38dcd7f7a381c4e3b225db9
Guazzaroni Leo <leo.guazzaroni@epita.fr>
2023-12-08 18:29:58 +0100

    feat: adding the base react app with the 3D background

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <div>
+      <Spline scene="https://prod.spline.design/Mh6polbJfdV2zwSH/scene.splinecode" />
+    </div>
+  );
+}

337e150f13126027257a0551fc2f38a3948426df
Guazzaroni Leo <leo.guazzaroni@epita.fr>
2023-12-09 13:02:51 +0100

    feat: creating the welcome page by adding the button get started

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-
-
+//import NavBar from './misc/Navbar';
 import Spline from '@splinetool/react-spline';
-
+import GetStartedButton from './welcome-page/components/GetStartedButton';
 export default function App() {
   return (
-    <div>
-      <Spline scene="https://prod.spline.design/Mh6polbJfdV2zwSH/scene.splinecode" />
+    <div className="Welcome-page">
+      <div className="content-wrapper">
+        <GetStartedButton></GetStartedButton>
+        <Spline scene="https://prod.spline.design/Mh6polbJfdV2zwSH/scene.splinecode">
+        </Spline>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/welcome-page/components/GetStartedButton.js
@@ -0,0 +1,26 @@
+import './button.css';
+
+export default function GetStartedButton() {
+    return (
+        <div className='GetStartedButton'>
+                <a href="javascript:;">
+                    <svg className="icon-arrow before">
+                        <use xlinkHref="#arrow"></use>
+                            </svg>
+                                <span className="label">Get Started</span>
+                            <svg className="icon-arrow after">
+                        <use xlinkHref="#arrow"></use>
+                    </svg>
+             </a>
+
+            <svg style={{ display: 'none' }}>
+                <defs>
+                    <symbol id="arrow" viewBox="0 0 35 15">
+                        <title>Arrow</title>
+                        <path d="M27.172 5L25 2.828 27.828 0 34.9 7.071l-7.07 7.071L25 11.314 27.314 9H0V5h27.172z" />
+                    </symbol>
+                </defs>
+            </svg>
+        </div>
+    )
+}

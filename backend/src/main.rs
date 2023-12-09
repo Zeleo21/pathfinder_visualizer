fcc91186b0279167390cb1cb60db3b54dc662bd8
Guazzaroni Leo <leo.guazzaroni@epita.fr>
2023-12-09 16:49:18 +0100

    feat: added the beginning of the maze creator.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,29 +1,35 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+// use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
+mod maze;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+// #[get("/")]
+// async fn hello() -> impl Responder {
+//     HttpResponse::Ok().body("Hello world!")
+// }
+
+// #[post("/echo")]
+// async fn echo(req_body: String) -> impl Responder {
+//     HttpResponse::Ok().body(req_body)
+// }
+ 
+// async fn manual_hello() -> impl Responder {
+//     HttpResponse::Ok().body("Hey there!")
+// }
+
+fn main() {
+    maze::mazegenerator::visualize_maze();
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("127.0.0.1", 7070))?
-    .run()
-    .await
-}
+// #[actix_web::main]
+// async fn main() -> std::io::Result<()> {
+//     maze::mazegenerator::maze_generator();
+//     HttpServer::new(|| {
+//         App::new()
+//             .service(hello)
+//             .service(echo)
+//             .route("/hey", web::get().to(manual_hello))
+//     })
+//     .bind(("127.0.0.1", 7070))?
+//     .run()
+//     .await
+// }

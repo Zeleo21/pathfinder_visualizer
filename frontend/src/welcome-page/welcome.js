dc7818eb226be5517d3853d829d2e30ffc37980e
Guazzaroni Leo <leo.guazzaroni@epita.fr>
2023-12-12 19:01:01 +0100

    feat: changed the 3D object to a donut?

--- frontend/src/welcome-page/welcome.js
+++ frontend/src/welcome-page/welcome.js
@@ -1,5 +1,5 @@
 import './../App.css';
-//import Spline from '@splinetool/react-spline';
+import Spline from '@splinetool/react-spline';
 import GetStartedButton from './components/GetStartedButton';
 
 export default function Welcome() {
@@ -7,8 +7,7 @@ export default function Welcome() {
     <div className="Welcome-page">
         <div className="content-wrapper">
           <GetStartedButton></GetStartedButton>
-          {/* <Spline scene="https://prod.spline.design/Mh5polbJfdV2zwSH/scene.splinecode">
-          </Spline> */}
+          <Spline scene="https://prod.spline.design/Y3EOcJ4BYJ-IFVCZ/scene.splinecode" />
         </div>
       </div>
     );
